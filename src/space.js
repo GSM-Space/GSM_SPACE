e20df4861d38af42cf6e38286bd3e291ea97f79a
seolin <iseolin76@gmail.com>
2020-11-04 09:00:41 +0900

    [MOVE] space.js

---
+++ src/space.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import logo from "./Asset/logo.png";
+
+function space() {
+  return (
+    <div>
+      <img src={logo} alt="로고"></img>
+      <h1>“ 학교생활을 하면서 불편한 점은
+      GSM SPACE로 청원해주세요 ! ” </h1>
+    </div>
+  )
+}
+
+export default space;
