8f2de51c8c764ae63e383c6f59d3d2f3bb9729d9
seolin <iseolin76@gmail.com>
2020-11-04 09:03:27 +0900

    [UPDATE] Route exact path component

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import space from "./space"
 
 const Router = () => {
-    return(
+    return (
         <>
-        <Route exact path="/" component={() => <div>메인 페이지입니다.</div>}/>
-        <Route path="/petition" component={() => <div>청원 폼입니다.</div>}/>
-        <Route path="/:state/petition" component={() => <div>청원 상태입니다.</div>}/>
+            <Route exact path="/" component={space} />
+            <Route path="/petition" component={() => <div>청원 폼입니다.</div>} />
+            <Route path="/:state/petition" component={() => <div>청원 상태입니다.</div>} />
         </>
     );
 }
