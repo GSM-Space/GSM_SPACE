1e5a7d3fa756c0c96f679de8673d7b1997cc2374
an0304073@naver.com <an0304073@naver.com>
2020-11-09 20:49:26 +0900

    [CREATE] Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Logo from 'assets/svg/Logo.js';
+import Search from 'assets/svg/Search.js';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>헤더</div>
+        <div className="header-container">
+            <Logo/>
+            <div className="header-input--wrapper">
+                <Search style={{ marginBottom: 10 }}/>
+                <input className="search-input" placeholder="검색어를 입력하세요"/>
+            </div>
+            <div className="welcome-box">
+
+            </div>
+        </div>
     );
 }
 
