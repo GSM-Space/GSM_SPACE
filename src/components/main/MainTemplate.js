1b628e137c48b1196dfb83709c9e73b93b892882
an0304073@naver.com <an0304073@naver.com>
2020-11-09 17:27:08 +0900

    [UPDATE] .png to .svg

--- src/components/main/MainTemplate.js
+++ src/components/main/MainTemplate.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from 'assets/logo.png';
-import indexImg from "assets/index-img.png";
+import Logo from 'assets/svg/Logo.js';
+import Index from 'assets/index-img.png';
 import PopUpTemplate from 'components/modal/PopUpTemplate.js';
 import './MainTemplate.css';
 
@@ -15,10 +15,10 @@ const MainTemplate = () => {
     return (
         <div className="Background">
             <div className="main-header">
-                <Link to="/"><img src={logo} alt="로고" className="main-logo"/></Link>
+                <Link to="/"><Logo className="main-logo"/></Link>
                 <button className="login-button" onClick={CheckPopUp}>로그인</button>
             </div>
-                <img className="index_img" src={indexImg} alt="바탕이미지"></img>
+                <img src={Index} alt="이미지" className="index_img"/>
                 {PopUp ? <PopUpTemplate toggle={CheckPopUp} currentState="logout"/> : null}
         </div>
     );
--- src/components/modal/Auth/LoginPopUp.js
+++ src/components/modal/Auth/LoginPopUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from 'assets/logo.png';
-import key from 'assets/key-img.png';
-import Email from 'assets/email-img.png';
+import Key from 'assets/svg/Key.js';
+import Email from 'assets/svg/Email.js';
 import './LoginPopUp.css';
 
 const LoginPopUp = () => {
@@ -10,11 +10,11 @@ const LoginPopUp = () => {
                 <img src={logo} alt="로고" style={{ margin: 35 }}/>
                     <div className="modal_title">“ 학교생활을 하면서 불편한 점을 청원해주세요 ! ”</div>
                 <div className="input-wrapper">
-                    <img src={Email} alt="이메일" style={{ marginLeft: 5 }} />
+                    <Email style={{ marginLeft: 5 }}/>
                     <input className="email-input" placeholder="이메일"/>
                 </div>
                 <div className="input-wrapper">
-                    <img src={key} alt="패스워드" style={{ marginLeft: 5 }} />
+                    <Key style={{ marginLeft: 5 }}/>
                     <input className="email-input" placeholder="비밀번호"/>
                 </div>
                 <div className="Google-login">구글 계정으로 로그인 할거임</div>
