c8d0e3a88503f577996e4ac09f60ff71f398ae67
an0304073@naver.com <an0304073@naver.com>
2020-11-11 21:06:30 +0900

    [UPDATE] create PetitionPage Stucture

--- src/pages/petition/PetitionPage.js
+++ src/pages/petition/PetitionPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PetitionTemplate from 'components/petition/PetitionTemplate';
-
+import Header from 'components/header/Header';
 
 const PetitionPage = () => {
     return (
+        <>
+        <Header/>
         <PetitionTemplate/>
+        </>
     );
 }
 
