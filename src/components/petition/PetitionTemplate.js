bab0c31a88a4cb01a7c1d9605dff75e28154a060
an0304073@naver.com <an0304073@naver.com>
2020-11-12 21:20:10 +0900

    [UPDATE] Petition list body

--- src/components/petition/PetitionTemplate.js
+++ src/components/petition/PetitionTemplate.js
@@ -5,7 +5,17 @@ const PetitionTemplate = () => {
     return (
         <div className="body-container">
             <div className="list-container">
-            
+                <div className="list-header">
+                    <div className="kind-of-petition--selected">
+                        <div style={{ marginBottom: 17 }}>진행중인 청원</div>
+                    </div>
+                    <div className="kind-of-petition">만료된 청원</div>
+                    <div className="kind-of-petition">답변 대기중인 청원</div>
+                    <div className="kind-of-petition">답변된 청원</div>
+                </div>
+                <div className="petition-list">
+
+                </div>
             </div>
             <div className="sidebar-container">
             
