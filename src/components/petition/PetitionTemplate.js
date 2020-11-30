35b36004f5632198690631c36a9e28b65ee727ec
an0304073@naver.com <an0304073@naver.com>
2020-12-01 07:08:33 +0900

    [UPDATE] sidebar

--- src/components/petition/PetitionTemplate.js
+++ src/components/petition/PetitionTemplate.js
@@ -1,44 +1,38 @@
-import React from 'react';
-import AnsweredPetition from './answered/AnsweredPetition';
-import ExpiredPetition from './expired/ExpriedPetition';
-import OngoingPetition from './ongoing/OngoingPetition';
-import PendingPetition from './pending/PendingPetition';
-import './PetitionTemplate.css';
-
+import React from "react";
+import AnsweredPetition from "./answered/AnsweredPetition";
+import ExpiredPetition from "./expired/ExpriedPetition";
+import OngoingPetition from "./ongoing/OngoingPetition";
+import PendingPetition from "./pending/PendingPetition";
+import Sidebar from "../sidebar/Sidebar";
+import "./PetitionTemplate.css";
 
 const CurrentPath = () => {
-    const pathName = window.location.pathname;
-    const path = pathName.split('/');
-    
-    switch(path[2]) {
-        case "ongoing":
-            return <OngoingPetition/>;
-        case "expired":
-            return <ExpiredPetition/>;
-        case "pending":
-            return <PendingPetition/>;
-        case "answered":
-            return <AnsweredPetition/>;
-        default:
-            return <OngoingPetition/>;
-    }
-}
+  const pathName = window.location.pathname;
+  const path = pathName.split("/");
+
+  switch (path[2]) {
+    case "ongoing":
+      return <OngoingPetition />;
+    case "expired":
+      return <ExpiredPetition />;
+    case "pending":
+      return <PendingPetition />;
+    case "answered":
+      return <AnsweredPetition />;
+    default:
+      return <OngoingPetition />;
+  }
+};
 
 const PetitionTemplate = () => {
-    return (
-        <div className="body-container">
-            <div className="list-container">
-                <div className="list-header">
-                    {CurrentPath()}
-                </div>
-                <div className="petition-list">
-                    
-                </div>
-            </div>
-            <div className="sidebar-container">
-            </div>
-        </div>
-    );
-}
-;
+  return (
+    <div className="body-container">
+      <div className="list-container">
+        <div className="list-header">{CurrentPath()}</div>
+        <div className="petition-list"></div>
+      </div>
+      <Sidebar />
+    </div>
+  );
+};
 export default PetitionTemplate;
