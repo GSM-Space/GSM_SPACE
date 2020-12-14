7148b956fc36adc427b573d43c5a0768528338e6
seolin <iseolin76@gmail.com>
2020-12-14 20:15:11 +0900

    [Add] Ongoing route path

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Petition, Main, Writing, Management, Post } from 'pages';
+import { Petition, Main, Writing, Management, Post, Ongoing } from 'pages';
 
 const Router = () => {
 	return (
@@ -8,6 +8,7 @@ const Router = () => {
 			<Route exact path="/" component={Main} />
 			<Route exact path="/petition/:status" component={Petition} />
 			<Route path="/petition/write" component={Writing} />
+			<Route path="/ongoing" component={Ongoing} />
 			<Route path="/ongoing/management" component={Management} />
 			<Route path="/petition/:status/post/:id" component={Post} />
 		</>
