7ed0206acdbd07bbe82d7e0b3dd199d3dbd7bbba
an0304073@naver.com <an0304073@naver.com>
2020-12-15 20:47:29 +0900

    [ADD] Google-social-login

--- src/components/modal/Auth/LoginPopUp.js
+++ src/components/modal/Auth/LoginPopUp.js
@@ -2,8 +2,17 @@ import React from "react";
 import Logo from "assets/svg/Logo.js";
 import Key from "assets/svg/Key.js";
 import Email from "assets/svg/Email.js";
+import GoogleLogin from "react-google-login";
 import "./LoginPopUp.css";
 import { Link } from "react-router-dom";
+
+const responseSuccess = (res) => {
+  console.log(res);
+};
+
+const responseFail = (err) => {
+  console.error(err);
+};
 
 const LoginPopUp = () => {
   return (
@@ -20,7 +29,13 @@ const LoginPopUp = () => {
         <Key style={{ marginLeft: 5 }} />
         <input className="email-input" placeholder="비밀번호" />
       </div>
-      <div className="Google-login">구글 계정으로 로그인 할거임</div>
+      <GoogleLogin
+        className="Google-login"
+        clientId="171117162418-7r68jqp88ifvrrkihodu48ur2lvjg1va.apps.googleusercontent.com"
+        buttonText="Google 아이디로 로그인"
+        onSuccess={responseSuccess}
+        onFailure={responseFail}
+      />
       <div className="modal_footer">
         <div className="forgot_password">비밀번호를 잊으셨나요 ?</div>
         <Link to="/petition/ongoing" className="modal-login--button">
